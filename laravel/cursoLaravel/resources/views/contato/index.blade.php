4b0ce7cfdc961e38294175384ab399554a4bd777
Bruno <brunocoio@gmail.com>
2022-01-11 15:32:15 -0300

    aula 8

--- laravel/cursoLaravel/resources/views/contato/index.blade.php
+++ laravel/cursoLaravel/resources/views/contato/index.blade.php
@@ -1,10 +1,7 @@
+
 <h3>View index</h3>
 
 @foreach($contatos as $contato)
-<!-- array -->
-<!-- <p>{{$contato['nome']}}</p>
-<p>{{$contato['tel']}}</p> -->
-<!-- obj -->
-<p>{{$contato -> nome}}</p>
-<p>{{$contato -> tel}}</p>
+<p>{{$contato->nome}}</p>
+<p>{{$contato->tel}}</p>
 @endforeach
---
+++ laravel/cursoLaravel/resources/views/layout/site.blade.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+  <title>@yield('titulo')</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
