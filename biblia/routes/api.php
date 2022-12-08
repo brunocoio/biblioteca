316a9d45c623a43d66fab1eb927c191bded8931f
Bruno <brunocoio@gmail.com>
2022-12-08 15:14:23 -0300

    api rest laravel

---
+++ biblia/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\TestamentoController;
+use App\Http\Controllers\LivroController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/testamento', [TestamentoController::class, 'index']);
+Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
+Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
+Route::post('/testamento', [TestamentoController::class, 'store']);
+Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
+
+Route::get('/livro', [LivroController::class, 'index']);
+Route::get('/livro/{id}', [LivroController::class, 'show']);
+Route::put('/livro/{id}', [LivroController::class, 'update']);
+Route::post('/livro', [LivroController::class, 'store']);
+Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
